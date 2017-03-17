4f391b9a4f599d6f0a16cb7e56aed1079082b080
Michael Jenkins <michael.jenkins@disney.com>
2017-03-17 11:31:50 -0700

    Update test script

--- Test/test.sh
+++ Test/test.sh
@@ -3,5 +3,7 @@ if [[ $# -eq 0 ]];
 then
     exit 0
 fi
-echo $(../Bash/weather $1)
-echo $(../Python/weather $1)
+echo "Bash   : $(../Bash/weather $1)"
+echo "Python : $(../Python/weather $1)"
+echo "Perl   : TBD"
+echo "Go     : TBD"

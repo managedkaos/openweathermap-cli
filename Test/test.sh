122217b92459e7444993483fbadbdf0cd865b359
Michael Jenkins <michael.jenkins@disney.com>
2017-03-17 15:36:40 -0700

    Update test script to print time data

--- Test/test.sh
+++ Test/test.sh
@@ -3,7 +3,31 @@ if [[ $# -eq 0 ]];
 then
     exit 0
 fi
-echo "Bash   : $(../Bash/weather $1)"
-echo "Python : $(../Python/weather $1)"
-echo "Perl   : TBD"
+
+# set options for time
+timeopts=''
+
+# get a temp file to hold the time data
+tempfile="${0}_$(date +"%s").txt"
+
+# call each solution and capture the output and time data
+# bash
+bash_output=$(/usr/bin/time $timeopts ../Bash/weather $1 2>$tempfile)
+bash_time=$(<$tempfile)
+
+# python
+python_output=$(/usr/bin/time $timeopts ../Python/weather $1 2>$tempfile)
+python_time=$(<$tempfile)
+
+# perl
+perl_output=$(/usr/bin/time $timeopts ../Perl/weather $1 2>$tempfile)
+perl_time=$(<$tempfile)
+
+# clean up the temp file
+rm -f $tempfile
+
+# print the report
+echo "Bash   : $bash_output $bash_time"
+echo "Python : $python_output $python_time"
+echo "Perl   : $perl_output $python_time"
 echo "Go     : TBD"
